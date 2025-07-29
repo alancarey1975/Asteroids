05a4714e3b47e4d860d1a4980ec6f662c2352e0b
Alan Carey <32581501+alancarey1975@users.noreply.github.com>
2025-07-29 17:26:13 +0100

    Init pygame and set screen

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # Initialise pygame
+    pygame.init()
+    # Initialise screen to given dimensions in a GUI window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Infinite loop to draw game on screen
+    while True:
+        # Check if the user has closed the window, if so, exit the game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black") # Fill the screen with solid black
+        pygame.display.flip() # Refresh the screen
 
 if __name__ == "__main__":
     main()
