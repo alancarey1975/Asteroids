bc92403dca937a2d21494386644b59d2b45e41e9
Alan Carey <32581501+alancarey1975@users.noreply.github.com>
2025-07-29 23:20:42 +0100

    Create Player class and draw object on screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init() # Initialise pygame
@@ -8,6 +9,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock() # Initialise clock for screen
     dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2) # Initialise a player in the middle of the screen
 
     # Infinite loop to draw game on screen
     while True:
@@ -17,6 +19,7 @@ def main():
                 return
 
         screen.fill("black") # Fill the screen with solid black
+        player.draw(screen) # Draw the player object on the screen
         pygame.display.flip() # Refresh the screen
         dt = clock.tick(60) / 1000 # Limit framerate to 60 FPS
 
